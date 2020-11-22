a74aedea032cfc56fa989de99002ff03c560f302
yash312312 <yashsrivastava3@gmail.com>
2020-11-23 00:15:17 +0530

    1)Replaced 'Usage' with 'Size' as it made more sense 2)Now copy paste path of your directory in the command line when asked for input 3)Opens generated csv as soon as it is generated

--- main.py
+++ main.py
@@ -2,30 +2,35 @@
 import sys
 import os
 import pandas as pd
-def get_size(path):
-	total = 0
-	for entry in os.scandir(path):
-		try:
-			if entry.is_dir(follow_symlinks=False):
-				total += get_size(entry.path)
-			else:
-				total += entry.stat(follow_symlinks=False).st_size
-		except Exception as e:
-			print(f"Exception : {e}")
-			total += 0
-	return total
 
-if __name__=='__main__':
-	path = '/home'
-	dir = sys.argv[1] if len(sys.argv)>1 else path
-	usage=[]
-	paths=[]
-	for entry in os.scandir(dir):
-		if (entry.is_dir(follow_symlinks=False)):
-			total=get_size(entry.path)/(1024*1024)
-			#print(entry.path,total)
-			paths.append(entry.path)
-			usage.append(total)
-	usage_dict = {'Directory':paths,'Usage(Mb)':usage}
-	df=pd.DataFrame(usage_dict)
-	df.to_csv("DiskReport.csv")
+
+def get_size(path):
+    total = 0
+    for entry in os.scandir(path):
+        try:
+            if entry.is_dir(follow_symlinks=False):
+                total += get_size(entry.path)
+            else:
+                total += entry.stat(follow_symlinks=False).st_size
+        except Exception as e:
+            print(f"Exception : {e}")
+            total += 0
+    return total
+
+
+if __name__ == '__main__':
+    print("Copy and Paste your directory path")
+    path = input()
+    dir = sys.argv[1] if len(sys.argv) > 1 else path
+    size = []
+    paths = []
+    for entry in os.scandir(dir):
+        if (entry.is_dir(follow_symlinks=False)):
+            total = get_size(entry.path) / (1024 * 1024)
+            # print(entry.path,total)
+            paths.append(entry.path)
+            size.append(total)
+    size_dict = {'Directory': paths, 'Size(Mb)': size}
+    df = pd.DataFrame(size_dict)
+    df.to_csv("DiskReport.csv",index_label="S.No")
+    os.startfile("DiskReport.csv")
